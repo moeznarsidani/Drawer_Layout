951c48f35b9417ee4f723199a9423156176cc4e9
Moez Firozbhai Narsidani <c0716607@mylambton.ca>
2018-11-13 14:25:09 -0500

    Add project files.

---
+++ drawerlayout/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Support.Design.Widget;
+using System;
+using Android.Views;
+using SupportActionBar = Android.Support.V7.App.ActionBar;
+
+using Toolbar = Android.Support.V7.Widget.Toolbar;
+
+namespace drawerlayout
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/MyTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private Android.Support.V4.Widget.DrawerLayout drawerLayout;
+        private NavigationView navView;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+            SupportActionBar.SetHomeButtonEnabled(true);
+            //SupportActionBar.SetHomeAsUpIndicator(Resource.Id.right_icon);
+            drawerLayout = FindViewById<Android.Support.V4.Widget.DrawerLayout>(Resource.Id.drawer_layout);
+            navView = FindViewById<NavigationView>(Resource.Id.nav_view);
+
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch(item.ItemId)
+            {
+                case Resource.Id.home:
+                    drawerLayout.OpenDrawer(Android.Support.V4.View.GravityCompat.Start);
+                    return true;
+            }
+            return base.OnOptionsItemSelected(item);
+
+        }
+    }
+}
